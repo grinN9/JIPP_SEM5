b1d9e02e6a8af610e70115a659f6da6e2daff4db
grinN9 <257671042+grinN9@users.noreply.github.com>
2026-01-28 04:47:59 +0100

    Create zmienne.py

---
+++ zadanie1/zmienne.py
@@ -0,0 +1,14 @@
+# 1) Najpierw utwórz zmienną o nazwie x i przypisz jej wartość 10
+x = 10
+
+# 2) Następnie utwórz drugą zmienną y i przypisz jej wartość 55
+y = 55
+
+# 3) Utwórz zmienną z i przypisz jej wartość x + y
+z = x + y
+
+# 4) Zastąp x wartością z
+x = z
+
+# 5) Wydrukuj wartość x za pomocą funkcji Pythona print()
+print(x)
